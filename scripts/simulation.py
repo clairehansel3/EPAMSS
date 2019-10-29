fixed gamma scaling

scripts/simulation.py
@@ -247,12 +247,12 @@ class Simulation(object):
             self['actual_analysis_points'])
         self.gamma = self['gamma_initial'] + self['gamma_prime'] * self.z
         self.bennett_radius = self['bennett_radius_initial'] * ((self.gamma /
-            self['gamma_initial']) ** (-1/4))
+            self['gamma_initial']) ** (-1/2))
         self.bennett_radius_si = self['bennett_radius_initial_si'] * ((
-            self.gamma / self['gamma_initial']) ** (-1/4))
-        self.rho_ion_div_n0 = self['rho_ion_div_n0_initial'] * np.sqrt(
+            self.gamma / self['gamma_initial']) ** (-1/2))
+        self.rho_ion_div_n0 = self['rho_ion_div_n0_initial'] * (
             self.gamma / self['gamma_initial'])
-        self.rho_ion_si = self['rho_ion_initial_si'] * np.sqrt(self.gamma /
+        self.rho_ion_si = self['rho_ion_initial_si'] * (self.gamma /
             self['gamma_initial'])
         self.means, self.covariance_matrix = getStatistics(self._output_dict)
         if read_phase_space and self['output_phase_space']:
@@ -269,13 +269,13 @@ class Simulation(object):
 
     def plot2DEmittance(self, filename='results/2Demittance.png'):
         plt.plot(self.z_si, self.emittances_2d_si[0, 0, :] * 1e9,
-            label='x (no scattering)', color='blue')
+            label='x (scattering off)', color='blue')
         plt.plot(self.z_si, self.emittances_2d_si[0, 1, :] * 1e9,
-            label='y (no scattering)', color='green')
+            label='y (scattering off)', color='green')
         plt.plot(self.z_si, self.emittances_2d_si[1, 0, :] * 1e9,
-            label='x (scattering)', color='red')
+            label='x (scattering on)', color='red')
         plt.plot(self.z_si, self.emittances_2d_si[1, 1, :] * 1e9,
-            label='y (scattering)', color='orange')
+            label='y (scattering on)', color='orange')
         plt.xlim(0, self['plasma_length_si'])
         plt.xlabel(r'$z$ (m)')
         plt.ylabel(r'$\epsilon_N$ (nm)')
@@ -365,15 +365,15 @@ class Simulation(object):
                 plt.title(r'z = {} m'.format(self.z_si[j]))
                 plt.hist(r * self['plasma_skin_depth_si'], density=True,
                     bins=bins, range=(0, r_div_a_max *
-                    self.bennett_radius_si[0]))
+                    self.bennett_radius_si[j]))
                 plt.plot(r_values * self['plasma_skin_depth_si'], r_pdf *
                     self['plasma_angular_wavenumber_si'], label='modified')
                 plt.plot(r_values * self['plasma_skin_depth_si'],
                     r_pdf_unmodified * self['plasma_angular_wavenumber_si'],
                     label='unmodified')
                 plt.xlabel(r'$r$ [m]')
-                plt.xlim(0, r_div_a_max * self.bennett_radius_si[0])
-                plt.ylim(0, 35000000)
+                plt.xlim(0, r_div_a_max * self.bennett_radius_si[j])
+                #plt.ylim(0, 35000000 * 2)
                 plt.legend()
                 plt.gca().get_xaxis().get_major_formatter().set_powerlimits(
                     (0, 1))
@@ -398,13 +398,13 @@ class Simulation(object):
                 # plot r' distribution
                 vr_values = np.linspace(-4 * sigma[0], 4 * sigma[0], points)
                 plt.title(r'z = {} m'.format(self.z_si[j]))
-                plt.hist(vr, density=True, bins=bins, range=(-4 * sigma[0],
-                    4 * sigma[0]))
+                plt.hist(vr, density=True, bins=bins, range=(-4 * sigma[j],
+                    4 * sigma[j]))
                 plt.plot(vr_values, np.exp(-vr_values ** 2 / (2 * sigma[j] ** \
                     2)) / (np.sqrt(2 * np.pi) * sigma[j]))
                 plt.xlabel(r'$\frac{dr}{dz}$ [unitless]')
-                plt.xlim(-4 * sigma[0], 4 * sigma[0])
-                plt.ylim(0, 7000)
+                plt.xlim(-4 * sigma[j], 4 * sigma[j])
+                #plt.ylim(0, 7000 * 2)
                 plt.gca().get_xaxis().get_major_formatter().set_powerlimits(
                     (0, 1))
                 plt.gca().get_yaxis().get_major_formatter().set_powerlimits(
@@ -414,13 +414,13 @@ class Simulation(object):
                 # plot r' distribution
                 rvth_values = np.linspace(-4 * sigma[0], 4 * sigma[0], points)
                 plt.title(r'z = {} m'.format(self.z_si[j]))
-                plt.hist(rvth, density=True, bins=bins, range=(-4 * sigma[0], 4 *
-                    sigma[0]))
+                plt.hist(rvth, density=True, bins=bins, range=(-4 * sigma[j], 4 *
+                    sigma[j]))
                 plt.plot(rvth_values, np.exp(-rvth_values ** 2 / (2 * sigma[j] ** \
                     2)) / (np.sqrt(2 * np.pi) * sigma[j]))
                 plt.xlabel(r'$r \frac{d\theta}{dz}$ [unitless]')
-                plt.xlim(-4 * sigma[0], 4 * sigma[0])
-                plt.ylim(0, 7000)
+                plt.xlim(-4 * sigma[j], 4 * sigma[j])
+                #plt.ylim(0, 7000 * 2)
                 plt.gca().get_xaxis().get_major_formatter().set_powerlimits(
                     (0, 1))
                 plt.gca().get_yaxis().get_major_formatter().set_powerlimits(

tests/scattering/scattering.py
@@ -14,98 +14,112 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#!/usr/bin/env python3
+# Copyright (C) 2019 Claire Hansel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import numpy as np
 import simulation
 import subprocess
+import sys
 
-rerun = True
+def parameterDefaults(run_name):
+    return {
+        'rho_ion_initial_si': 1e26,
+        'plasma_length_si': 0.01,
+        'beam_energy_initial_gev': 10,
+        'acceleration_gradient_gev_per_m': 0,
+        'bennett_radius_initial_si': 2.5e-8,
+        'cross_section_radius_si': 1e-8,
+        'unperturbed_plasma_density_si': 1e24,
+        'integration_tolerance': 1e-10,
+        'vartheta_cutoff': 10,
+        'ion_atomic_number': 1,
+        'seed': 1563489705,
+        'minimum_steps_per_betatron_period': 100,
+        'particles_target': 100,
+        'analysis_points_target': 100,
+        'spline_points': 1000,
+        'max_order': 3,
+        'max_integration_depth': 15,
+        'output_filename': 'data/{}_output'.format(run_name),
+        'statistics_filename': 'data/{}_statistics'.format(run_name),
+        'phase_space_filename': 'data/{}_phase_space'.format(run_name),
+        'output_phase_space': True,
+        'modified_bennett': True
+    }
 
-def main1():
-    compute_processes = 1
-    sims = []
-    labels = []
+def run(run_name='run1', parameters={}, hoffman2=False, compute_processes=1):
+    parameter_defaults = parameterDefaults(run_name)
+    assert all(key in parameter_defaults for key in parameters)
+    with open('data/{}_input'.format(run_name), 'w') as f:
+        for key in parameter_defaults.keys():
+            if key in parameters:
+                value = parameters[key]
+            else:
+                value = parameter_defaults[key]
+            f.write('{} = {}\n'.format(key, value))
+    if hoffman2:
+        subprocess.run(
+            ['qsub', '-pe', 'dc_*', str(compute_processes + 1), 'run_epamss.sh', 'data/{}_input'.format(run_name)],
+            check=True
+        )
+    else:
+        subprocess.run(
+            ['mpirun', '-np', str(compute_processes + 1), 'epamss', 'data/{}_input'.format(run_name)],
+            check=True
+        )
+    #return simulation.Simulation(parameters['output_filename'] if 'output_filename' in parameters else parameter_defaults['output_filename'])
+
+
+def submitjobs():
     exponents = np.arange(-10, -5)
     for run_number, exponent in enumerate(exponents):
-        run = 'rsigma_{}'.format(run_number)
-        cross_section_radius_si = 10. ** exponent
-        if rerun:
-            parameters = {
-                'rho_ion_initial_si': 9.43718e23, # irrelevant
-                'plasma_length_si': 100,
-                'beam_energy_initial_gev': 10,
-                'acceleration_gradient_gev_per_m': 0,
-                'bennett_radius_initial_si': 2.37296e-7, # irrelevant
-                'cross_section_radius_si': cross_section_radius_si,
-                'unperturbed_plasma_density_si': 1e26,
-                'integration_tolerance': 1e-10,
-                'vartheta_cutoff': 10,
-                'ion_atomic_number': 1,
-                'minimum_steps_per_betatron_period': 100000, # steps
-                'particles_target': 1000,
-                'analysis_points_target': 1000,
-                'spline_points': 1000,
-                'max_order': 3,
-                'max_integration_depth': 15,
-                'output_filename': 'data/output_{}'.format(run),
-                'statistics_filename': 'data/statistics_{}'.format(run),
-                'phase_space_filename': 'data/phase_space_{}'.format(run),
-                'output_phase_space': True,
-                'modified_bennett': True # irrelevant
-            }
-            with open('data/input_{}'.format(run), 'w') as f:
-                for key, value in parameters.items():
-                    f.write('{} = {}\n'.format(key, value))
-            subprocess.run(
-                ['mpirun', '-np', str(compute_processes+1), 'epamss',
-                'data/input_{}'.format(run)], check=True
-            )
-        sims.append(simulation.Simulation('data/output_{}'.format(run)))
-        labels.append(r'$r_{\sigma} = 10^{' + str(exponent) + '}$')
-    simulation.Simulation.plotScatteringTest(sims, labels, filename='results/rsigma.png')
-
-def main2():
-    compute_processes = 1
-    sims = []
-    labels = []
-    exponents = np.arange(3, 7)
-    for run_number, exponent in enumerate(exponents):
+        runname = 'rsigma_{}'.format(exponent)
+        run(runname, {'cross_section_radius_si': 10. ** exponent, 'minimum_steps_per_betatron_period': 100000}, hoffman2=True, compute_processes=3)
+    exponents2 = np.arange(3, 7)
+    for run_number, exponent in enumerate(exponents2):
         steps = 10 ** exponent
-        run = 'steps_{}'.format(run_number)
-        if rerun:
-            parameters = {
-                'rho_ion_initial_si': 9.43718e23, # irrelevant
-                'plasma_length_si': 100,
-                'beam_energy_initial_gev': 10,
-                'acceleration_gradient_gev_per_m': 0,
-                'bennett_radius_initial_si': 2.37296e-7, # irrelevant
-                'cross_section_radius_si': 1e-8,
-                'unperturbed_plasma_density_si': 1e26,
-                'integration_tolerance': 1e-10,
-                'vartheta_cutoff': 10,
-                'ion_atomic_number': 1,
-                'minimum_steps_per_betatron_period': steps, # steps
-                'particles_target': 1000,
-                'analysis_points_target': 1000,
-                'spline_points': 1000,
-                'max_order': 3,
-                'max_integration_depth': 15,
-                'output_filename': 'data/output_{}'.format(run),
-                'statistics_filename': 'data/statistics_{}'.format(run),
-                'phase_space_filename': 'data/phase_space_{}'.format(run),
-                'output_phase_space': True,
-                'modified_bennett': True # irrelevant
-            }
-            with open('data/input_{}'.format(run), 'w') as f:
-                for key, value in parameters.items():
-                    f.write('{} = {}\n'.format(key, value))
-            subprocess.run(
-                ['mpirun', '-np', str(compute_processes+1), 'epamss',
-                'data/input_{}'.format(run)], check=True
-            )
-        sims.append(simulation.Simulation('data/output_{}'.format(run)))
-        labels.append('$10^{' + str(exponent) + '}$ steps')
-    simulation.Simulation.plotScatteringTest(sims, labels, filename='results/steps.png')
+        runname = 'steps_{}'.format(run_number)
+        run(runname, {'cross_section_radius_si': 1e-8, 'minimum_steps_per_betatron_period': 10 ** exponent}, hoffman2=True, compute_processes=3)
+
+def analyze():
+    sims1 = []
+    labels1 = []
+    exponents = np.arange(-10, -5)
+    for run_number, exponent in enumerate(exponents):
+        runname = 'rsigma_{}'.format(exponent)
+        sims1.append(simulation.Simulation('data/{}_output'.format(runname)))
+        labels1.append(r'$r_{\sigma} = 10^{' + str(exponent) + '}$')
+    sims2 = []
+    labels2 = []
+    exponents2 = np.arange(3, 7)
+    for run_number, exponent in enumerate(exponents2):
+        steps = 10 ** exponent
+        runname = 'steps_{}'.format(run_number)
+        sims2.append(simulation.Simulation('data/{}_output'.format(runname)))
+        labels2.append('$10^{' + str(exponent) + '}$ steps')
+
+    simulation.Simulation.plotScatteringTest(sims1, labels1, filename='results/rsigma.png')
+    simulation.Simulation.plotScatteringTest(sims2, labels2, filename='results/steps.png')
+
+def main():
+    if len(sys.argv) > 1 and sys.argv[1] == 'run':
+        submitjobs()
+    elif len(sys.argv) > 1 and sys.argv[1] == 'analyze':
+        analyze()
 
 if __name__ == '__main__':
-    main1()
-    main2()
+    main()